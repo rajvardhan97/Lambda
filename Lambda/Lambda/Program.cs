e503fb456c49b8108c2bda09287b242690764792
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-06-03 15:49:41 +0530

    [Raj]Add: Skip record from the list for age is less than 60

--- Lambda/Lambda/Program.cs
+++ Lambda/Lambda/Program.cs
@@ -13,8 +13,8 @@ namespace Lambda
             PeopleList.Add(new Person(1, "Ram", "UP", 22));
             PeopleList.Add(new Person(8, "Akshay", "Punjab", 44));
             PeopleList.Add(new Person(21, "Arpita", "UK", 77));
+            PeopleList.Add(new Person(19, "Raj", "HP", 30));
             PeopleList.Add(new Person(5, "Shantanu", "Bihar", 62));
-            PeopleList.Add(new Person(4, "Raj", "HP", 22));
 
             Console.WriteLine("Retrieve top 2 records with age less than 60");
             foreach (Person p in PeopleList.FindAll(e => (e.age < 60)).Take(2).ToList())
@@ -37,6 +37,12 @@ namespace Lambda
             if (PeopleList.Exists(e => e.name == NAME))
             {
                 Console.WriteLine(NAME + " Exist in the list");
+            }
+
+            Console.WriteLine("\nSkip record from the list for age is less than 60");
+            foreach(Person person in PeopleList.SkipWhile(e => e.age < 60))
+            {
+                Console.WriteLine("Name: " + person.name + " Age: " + person.age);
             }
         }
     }
