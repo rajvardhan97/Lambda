23bf1b48d9a426f403df77973764a642d33772d3
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-06-03 13:27:10 +0530

    [Raj]Add: Create Person class and add default values

---
+++ Lambda/Lambda/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Person
+    {
+        public int SSN;
+        public string name;
+        public string address;
+        public int age;
+        public Person(int id, string name, string address, int age)
+        {
+            this.SSN = id;
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+    }
+}
---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lambda
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            List<Person> PeopleList = new List<Person>();
+            PeopleList.Add(new Person(2, "Harsh", "RJ", 17));
+            PeopleList.Add(new Person(1, "Ram", "UP", 22));
+            PeopleList.Add(new Person(8, "Akshay", "Punjab", 44));
+            PeopleList.Add(new Person(21, "Arpita", "UK", 77));
+            PeopleList.Add(new Person(5, "Shantanu", "Bihar", 62));
+            PeopleList.Add(new Person(4, "Raj", "HP", 22));
+        }
+    }
+}
